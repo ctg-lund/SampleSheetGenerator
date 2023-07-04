da11c00bc26f4769e16c76e036de64c6655fd5f3
Fattigman <jacob.karlstrom@gmail.com>
2023-07-04 15:26:53 +0200

    tests if singlecell sheet generator is working

--- test.py
+++ test.py
@@ -5,8 +5,11 @@ def read_samplesheets():
     samplesheet = pd.read_csv('data/singlecell/samplesheet.csv')
     feature_ref = pd.read_csv('data/singlecell/feature_ref.csv')
     flex_config = pd.read_csv('data/singlecell/flex_config.csv')
-    return samplesheet, feature_ref, flex_config
+    with open('data/singlecell/ctg_samplesheet.csv') as f: 
+        ctg_samplesheet = f.read()
+    return samplesheet, feature_ref, flex_config, ctg_samplesheet
 
 def test():
-    ss, fr, fc = read_samplesheets()
+    ss, fr, fc, ctg_samplesheet = read_samplesheets()
     samplesheet = singleCellSheet(ss, fc, fr, False) 
+    assert samplesheet.dataDf == ctg_samplesheet
