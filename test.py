6531f6d0deca68ea2a986bf990cee35707471b31
Fattigman <jacob.karlstrom@gmail.com>
2023-07-04 16:48:27 +0200

    Added command to see dir structure

--- test.py
+++ test.py
@@ -4,9 +4,11 @@ from os import environ
 
 def read_samplesheets():
     basepath = environ.get('GITHUB_WORKSPACE', '')
-    samplesheet = pd.read_csv(f'{basepath}/data/singlecell/samplesheet.csv')
-    feature_ref = pd.read_csv(f'{basepath}/data/singlecell/feature_ref.csv')
-    flex_config = pd.read_csv(f'{basepath}/data/singlecell/flex_config.csv')
+    if basepath != '':
+        basepath += '/'
+    samplesheet = pd.read_csv(f'{basepath}data/singlecell/samplesheet.csv')
+    feature_ref = pd.read_csv(f'{basepath}data/singlecell/feature_ref.csv')
+    flex_config = pd.read_csv(f'{basepath}data/singlecell/flex_config.csv')
     with open('data/singlecell/ctg_samplesheet.csv') as f: 
         ctg_samplesheet = f.read()
     return samplesheet, feature_ref, flex_config, ctg_samplesheet
