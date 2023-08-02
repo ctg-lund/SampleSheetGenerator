e1721b8096fb9a966c3b52505f1d5ac2dd829450
Fattigman <jacob.karlstrom@gmail.com>
2023-08-02 13:50:50 +0200

    formated code with Black

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from samplesheet import singleCellSheet
 import pandas as pd
 import os
 from os.path import join, normpath, isabs
+
+
 def abspath(path):
     """Return an absolute path."""
     path = os.fspath(path)
@@ -13,7 +15,10 @@ def abspath(path):
         path = join(cwd, path)
     return normpath(path)
 
-def read_samplesheets(samplesheet:str, feature_ref:str, flex_config:str, ctg_samplesheet: str) -> tuple:
+
+def read_samplesheets(
+    samplesheet: str, feature_ref: str, flex_config: str, ctg_samplesheet: str
+) -> tuple:
     samplesheet = pd.read_csv(abspath(samplesheet))
     feature_ref = pd.read_csv(abspath(feature_ref))
     flex_config = pd.read_csv(abspath(flex_config))
@@ -21,11 +26,13 @@ def read_samplesheets(samplesheet:str, feature_ref:str, flex_config:str, ctg_sam
         ctg_samplesheet = f.read()
     return samplesheet, feature_ref, flex_config, ctg_samplesheet
 
+
 def test_samplesheet():
-    ss, fr, fc, ctg_samplesheet = read_samplesheets('data/singlecell/SampleSheet.csv', 
-                                                    'data/singlecell/feature_ref.csv', 
-                                                    'data/singlecell/flex_config.csv',
-                                                    'data/singlecell/CTG_SampleSheet.csv'
-                                                    )
-    samplesheet = singleCellSheet(ss, fc, fr, False) 
+    ss, fr, fc, ctg_samplesheet = read_samplesheets(
+        "data/singlecell/SampleSheet.csv",
+        "data/singlecell/feature_ref.csv",
+        "data/singlecell/flex_config.csv",
+        "data/singlecell/CTG_SampleSheet.csv",
+    )
+    samplesheet = singleCellSheet(ss, fc, fr, False)
     assert samplesheet.dataDf == ctg_samplesheet
